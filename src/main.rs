393f3cbe1aa972a5f30c186f4875d80099a29f55
shilazi <sunshuaids@live.com>
2024-11-25 18:43:46 +0800

    Pass-through CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use clap::Parser;
+
 fn main() {
     env_logger::init_from_env(env_logger::Env::default().filter_or("WAIT_LOGGER_LEVEL", "debug"));
 
+    let parameter = wait::Parameter::parse();
     let mut sleep = wait::sleeper::new();
-    wait::wait(&mut sleep, &wait::config_from_env(), &mut on_timeout);
+    wait::wait(&mut sleep, &wait::config_from_env(parameter), &mut on_timeout);
 }
 
 fn on_timeout() {
